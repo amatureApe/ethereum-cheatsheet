9fd118941ac0ceec4f07d128085b7c4745d8b2cb
amatureApe <amatureApe@gmail.com>
2022-08-15 14:38:30 -0500

    add more examples

---
+++ solidity-examples/array-replace-last.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract ArrayReplaceLast {
+    uint256[] public arr = [1, 2, 3, 4];
+
+    function remove(uint256 _index) external {
+        // Write your code here
+        arr[_index] = arr[arr.length - 1];
+        arr.pop();
+    }
+}
